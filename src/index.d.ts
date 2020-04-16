218725c040a13afa9e68a5828ae459be72955036
Fela Maslen <felamaslen@gmail.com>
2020-04-16 15:09:44 +0100

    Non-optional state/action on partial reducer type

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,7 +3,7 @@ type Action = {
 };
 
 type Reducer<S> = (state?: S, action?: Action | null) => S;
-type PartialReducer<S> = (state?: S, action?: Action | null) => Partial<S>;
+type PartialReducer<S> = (state: S, action: Action) => Partial<S>;
 
 interface ReducerMap<S> {
   [actionType: string]: PartialReducer<S>;
